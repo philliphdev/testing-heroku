23168f03487472b84cb02275d35769094b474291
philliphdev <phannafo@us.ibm.com>
2018-06-05 11:27:52 -0400

    Setup DB connection

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+require('ditenv').config()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
